3022b0c1d03bcd1c94039c8c0a8f1e6172fc4731
Elizabeth Champagne <64558721+eachampagne@users.noreply.github.com>
2026-01-23 09:28:08 -0600

    change event type

--- server/routers/calendar.ts
+++ server/routers/calendar.ts
@@ -4,20 +4,34 @@ import express from 'express';
 // https://stackoverflow.com/questions/37688318/typescript-interface-possible-to-make-one-or-the-other-properties-required
 
 interface EventBase {
-  summary: string
+  summary: string,
+  id: string
+}
+
+interface AllDayTime {
+  date: string,
+  dateTime?: never,
+  timeZone?: never
+}
+
+interface PartDayTime {
+  date?: never,
+  dateTime: string,
+  timeZone: string
 }
 
 interface EventAllDay extends EventBase {
-  start: {date: string, dateTime?: never}
-  end: {date: string, dateTime?: never}
+  start: AllDayTime
+  end: AllDayTime
 }
 
 interface EventPartDay extends EventBase {
-  start: {dateTime: string, date?: never}
-  end: {dateTime: string, date?: never}
+  start: PartDayTime
+  end: PartDayTime
 }
 
 type Event = EventAllDay | EventPartDay;
+
 
 const router = express.Router();
 
@@ -34,6 +48,7 @@ router.get('/', async (req, res) => {
     const events: [Event] = response.data.items.map((event: Event) => {
       return {
         summary: event.summary,
+        id: event.id,
         start: event.start,
         end: event.end
       }
