06b8b54e173996a4cf204825bc48ef5b3fcfae27
Elizabeth Champagne <64558721+eachampagne@users.noreply.github.com>
2026-01-23 19:59:58 -0600

    calendar auth: direct to Google

--- server/routers/auth.ts
+++ server/routers/auth.ts
@@ -1,5 +1,8 @@
+import crypto from 'node:crypto';
 import express from 'express';
+import session from 'express-session';
 import passport from 'passport';
+import { google } from 'googleapis';
 import GoogleStrategy from 'passport-google-oidc';
 import { User } from '../../generated/prisma/client.js' // * 'User' Type.
 import { prisma } from '../database/prisma.js';
@@ -60,6 +63,39 @@ passport.deserializeUser(function(user : User, cb) {
   process.nextTick(function() {
     return cb(null, user);
   });
+});
+
+const oauth2Client = new google.auth.OAuth2(
+  process.env['GOOGLE_CLIENT_ID'],
+  process.env['GOOGLE_CLIENT_SECRET'],
+  process.env['CALENDAR_REDIRECT_URL']
+);
+
+const scopes = ['https://www.googleapis.com/auth/calendar.readonly'];
+
+
+router.get('/auth/calendar', (req, res) => {
+  const state = crypto.randomBytes(32).toString('hex');
+  
+  // store state in session - presumably this should happen in a specific request
+  req.session.state = state;
+  
+  const authorizationUrl = oauth2Client.generateAuthUrl({
+    access_type: 'online', // do I need offline?
+    scope: scopes,
+    include_granted_scopes: true,
+    state: state
+  });
+
+  res.redirect(authorizationUrl);
+});
+
+router.get('/auth/redirect/calendar', (req, res) => {
+  console.log('calendar redirect');
+
+  console.log(req.query);
+
+  res.redirect('/');
 });
 
 export default router;
