b634222bf3a25144eae7c459299e7ea090a5f481
JaylinJones0 <jonesjaylink@gmail.com>
2026-01-31 18:48:30 -0600

    (delete) scheduled-tasks directory

--- server/scheduled-tasks/dashboard-scheduler.ts
+++
@@ -1,9 +0,0 @@
-import cron from 'node-cron';
-
-import { Prisma } from '../../generated/prisma/client.js'; // not sure about this, although it matches what was in database/prisma
-import { User } from '../../generated/prisma/client.js'
-import { prisma } from '../database/prisma.js';
-
-cron.schedule('', () => {
-
-})
