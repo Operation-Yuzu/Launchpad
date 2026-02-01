fff5f037649afd83e1c9f1773e986acfe037d891
azaria <azariaplayer@gmail.com>
2026-01-31 19:03:05 -0600

    PATCH is working now. Verify is now in its own PATCH

--- server/routers/phoneNumbers.ts
+++ server/routers/phoneNumbers.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 
 import { prisma } from '../database/prisma.js';
+import { stat } from 'node:fs';
 
 const phoneNumbers = express.Router();
 
@@ -58,5 +59,85 @@ phoneNumbers.post('/:ownerId', async (req, res) => {
   }
 
 })
+
+// PATCH - need to make it to where if they want to change the number, they can
+// but also if they click a button or verify those fields are changed too
+phoneNumbers.patch('/:ownerId', async (req, res) => {
+  const { notifications, contactNumber} = req.body
+  try {
+    const existing = await prisma.phoneNumbers.findUnique({
+      where: {
+        userId: Number(req.params.ownerId)
+      }
+    })
+
+    // check if it exist
+    if(!existing){
+      res.status(404).send('You have nothing to update')
+    }
+
+    const data: {
+      contactNumber?: string,
+      verified?: boolean,
+      notifications?: boolean
+    } = {}
+    // if the contactNumber is provided - update the phone number
+    if(contactNumber){
+      data.contactNumber = contactNumber
+      data.verified = false
+      data.notifications = false
+    }
+
+    // if something is clicked change the boolean value
+    // if(typeof verified === 'boolean'){
+    //   data.verified = verified
+    // }
+
+    // if(typeof notifications === 'boolean'){
+    //   data.notifications = notifications
+    // }
+
+    data.notifications = !existing?.notifications
+
+    const status = await prisma.phoneNumbers.update({
+      where: {
+        userId: Number(req.params.ownerId)
+      },
+      data
+    })
+    res.status(201).send({'Successful': status})
+  } catch (error) {
+    res.status(500).send({'You have no number to update': error})
+  }
+})
+
+phoneNumbers.patch('/verify/:ownerId', async (req, res) => {
+  //const { verified } = req.body;
+  
+  try {
+    const existing = await prisma.phoneNumbers.findUnique({
+      where: {
+        userId: Number(req.params.ownerId)
+      }
+    })
+
+    if(existing?.verified === true){
+      res.status(404).send('This number is already verified')
+    }
+
+    await prisma.phoneNumbers.update({
+      where: {
+        userId: Number(req.params.ownerId)
+      },
+      data: {
+        verified : true
+      }
+    })
+    res.status(201).send('Success')
+  } catch (error) {
+      res.status(500).send({'Could not verify': error})
+    }
+  })
+
 
 export default phoneNumbers;
