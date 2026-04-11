c3b639325301db12dec62953d0ddcab05cc7306a
Elizabeth Champagne <64558721+eachampagne@users.noreply.github.com>
2026-04-10 23:01:55 -0500

    add support for rgba() colors to color utility

--- src/utilities/color.ts
+++ src/utilities/color.ts
@@ -1,11 +1,19 @@
 // need this file to make a font color changer function based on the background - nav - widget color
 
 
-const changeTextColor = (textColor?: string) => {
+const changeTextColor = (textColor?:string) => {
   if(!textColor){
-    return 'black'
+    return 'black';
   }
 
+  if (textColor.slice(0, 4) === 'rgba') {
+    return changeRGBAColor(textColor);
+  } else {
+    return changeHexColor(textColor);
+  }
+};
+
+const changeHexColor = (textColor: string) => {
   const hex = textColor.replace('#', '');
 
   const r = parseInt(hex.substring(0, 2), 16);
@@ -15,5 +23,17 @@ const changeTextColor = (textColor?: string) => {
   const brightness = (r * 299 + g * 587 + b * 114) / 1000;
   return brightness > 155 ? 'black' : 'white';
 }
+
+const changeRGBAColor = (rgba: string) => {
+  // I am going to assume that this is of the form rgba(#, #, #, #);
+
+  const values = rgba.slice(5, rgba.length - 2); // remove rgba( and )
+  // drops the a value
+  const [r, g, b] = values.split(',').map(value => Math.trunc(parseFloat(value) * 255)); // split on commas, convert values to 0-255 range instead of 0-1
+
+  const brightness = (r * 299 + g * 587 + b * 114) / 1000;
+
+  return brightness > 155 ? 'black' : 'white';
+};
 
 export default changeTextColor
